761c2756b0aad47abcb0a05a659bf12ad95b80e4
NickHoch <kolya.goch@gmail.com>
2019-03-26 17:45:14 +0200

    Add project files.

---
+++ InitHotelDb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InitHotelDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
